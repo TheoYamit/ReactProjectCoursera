d295538f48d42ce147d2eb85a0cd2cd8d6873141
Theodore Yamit <116919917+TheoYamit@users.noreply.github.com>
2024-08-19 17:05:01 -0600

    Added more to the files

---
+++ my-app/src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { useState } from 'react';
+import { Flex } from '@chakra-ui/react';
+
+const Header = () => {
+
+  return (
+    <header>
+      <img ></img>
+    </header>
+  )
+}
+
+export default Header;
+
