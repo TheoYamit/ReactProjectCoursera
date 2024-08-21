fa59f3263d2d9b5b42ed744a91586cdc9e8f4f27
Theodore Yamit <116919917+TheoYamit@users.noreply.github.com>
2024-08-21 15:52:35 -0600

    Finished designing hero

--- CapstoneProjectCoursera/src/components/Hero/index.jsx
+++ CapstoneProjectCoursera/src/components/Hero/index.jsx
@@ -1,17 +1,24 @@
 import React from 'react';
-import { HStack, VStack, Box, Text } from '@chakra-ui/react';
+import './hero.css'
+import { HStack, VStack, Box, Text, Image, Button, Spacer } from '@chakra-ui/react';
+import heroImage from '../../assets/hero.jpg';
 
 const Hero = () => {
 
   return (
     <>
-      <Box w="100%" backgroundColor="rgb(180, 180, 180, 0.5)">
+      <Box className="hero-main-box" w="100%" backgroundColor="rgb(180, 180, 180, 0.5)">
         <HStack>
-          <VStack>
+          <VStack align="start">
             <Text fontSize="5xl">Little Lemon</Text>
+            <Text fontSize="2xl"> Chicago </Text>
+            <Text fontSize="2xl">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur nec magna odio. Nam fringilla tellus ut justo bibendum tempor.</Text>
+            <Spacer/>
+            
+            <Button size="lg" backgroundColor="rgba(60, 60, 60, 0.9)" color="white"> Reserve a Table </Button>
           </VStack>
           <Box>
-
+            <Image src={heroImage}/>
           </Box>
         </HStack>
       </Box>
