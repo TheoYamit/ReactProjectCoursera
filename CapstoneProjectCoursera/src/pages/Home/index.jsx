6cb9a263ade67399de5dcd4bd544d86c3e3e97bb
Theodore Yamit <116919917+TheoYamit@users.noreply.github.com>
2024-08-22 00:35:38 -0600

    Starting specials

--- CapstoneProjectCoursera/src/pages/Home/index.jsx
+++ CapstoneProjectCoursera/src/pages/Home/index.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import Navbar from '../../components/Navbar';
 import About from '../../components/HomeComponents/About';
 import Hero from '../../components/Hero';
+import Highlights from '../../components/Highlights';
 const Home = () => {
 
   return (
     <>
       <Navbar/>
       <Hero/>
+      <Highlights/>
     </>
   )
 }
