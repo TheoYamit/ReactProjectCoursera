9b2ebff816ef0abc677f9f4f4af5be8b430db631
Theodore Yamit <116919917+TheoYamit@users.noreply.github.com>
2024-08-25 21:47:57 -0600

    Setting up a reducer

--- CapstoneProjectCoursera/src/components/BookingForm/index.jsx
+++ CapstoneProjectCoursera/src/components/BookingForm/index.jsx
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './bookingform.css'
-import { FormControl, FormLabel, FormErrorMessage, FormHelperText, Box, 
-  Input, Text, Image, HStack, VStack } from '@chakra-ui/react';
+import { Flex, FormControl, FormLabel, FormErrorMessage, FormHelperText, Box, 
+  Input, Text, Image, HStack, VStack, Button, Select } from '@chakra-ui/react';
 import reservedPic from '../../assets/reserved.jpg'
-const BookingForm = () => {
+const BookingForm = (props) => {
+
+  const availableTimes = props.availableTimes
+
+  useEffect(() => {
+    console.log(availableTimes)
+  }, [availableTimes])
+
+  const [formDetails, setFormDetails] = useState({
+    date: null,
+    time: null,
+    numOfGuests: null,
+    ocassion: null
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormDetails(prevState => ({
+      ...prevState,
+      [name]: value
+    }));
+  };
+
+  useEffect(() => {
+    console.log(formDetails)
+  }, [formDetails])
+
+
+
+
   return (
     <>
       <div className='booking-padding-first'>
@@ -18,8 +47,43 @@ const BookingForm = () => {
         </HStack>
       </div>
 
+      <Text paddingTop='100px'textAlign='center' fontSize='5xl'> Reservation Form </Text>
+
       <Box as='form' className='booking-padding-second'>
 
+        <FormControl paddingBottom={10}>
+          <FormLabel> Date </FormLabel>
+          <Input onChange={handleInputChange} name='date' type='date'></Input>
+        </FormControl>
+
+        <FormControl paddingBottom={10}>
+          <FormLabel> Time </FormLabel>
+          <Select onChange={handleInputChange} name='time' placeholder='Select a timeslot'>
+            {availableTimes.map((time, index) => {
+                return (
+                  <option key={index} value={time}>{time}</option>
+                )
+            })};
+          </Select>
+        </FormControl>
+        
+        <FormControl paddingBottom={10}>
+          <FormLabel> Number of Guests</FormLabel>
+          <Input onChange={handleInputChange} name='numOfGuests' type='number' placeholder='1' min='1' max='10'></Input>
+        </FormControl>
+
+        <FormControl paddingBottom={10}>
+          <FormLabel> Occassion </FormLabel>
+          <Select onChange={handleInputChange} name='ocassion' placeholder='Select the occasion'>
+            <option> Birthday </option>
+            <option> Anniversary </option>
+            <option> Other </option>
+          </Select>
+        </FormControl>
+
+        <Flex justifyContent='center'>
+          <Button> Reserve </Button>
+        </Flex>
       </Box>
     </>
   );
--- CapstoneProjectCoursera/src/pages/BookingPage/index.jsx
+++ CapstoneProjectCoursera/src/pages/BookingPage/index.jsx
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Navbar from '../../components/Navbar';
 import BookingForm from '../../components/BookingForm';
+
+const initializeTimes = () => ['17:00', '18:00', '19:00', '20:00', '21:00', '22:00'];
+
+  const timesReducer = (state, action) => {
+    switch (action.type) {
+      case 'UPDATE_TIMES':
+        return action.payload;
+      default:
+        return state;
+    }
+  };
+
+
 const Booking = () => {
+  const [availableTimes, dispatch] = useReducer(timesReducer, [], initializeTimes);
 
   return (
     <>
       <Navbar/>
-      <BookingForm/>
+      <BookingForm availableTimes={availableTimes} dispatch={dispatch}/>
     </>
   );
 };
